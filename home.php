89020c23480a565150338b92535aa6dbd24f8383
joel2035 <joel.pokam@hetic.net>
2018-06-27 01:51:26 +0200

    loisir & home

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php include('header.php'); ?>
+<section class="home">
+<div class="container">
+  <div class="row ">
+
+
+    <div class="home-content-logo">
+
+ <img class="home-content-img" src="../assets/img/logo.png" alt="">    </div>
+  </div>
+<div class="col-xs-12">
+<h1 class="home-content-title"> Bienvenue sur <span class="home-content-title-span">Iscarion</span> </h1>
+<h2 class="home-content-bar"></h2>
+<p class="home-content-text">Iscaryon est la plus grande place marchande de la galaxie !</p>
+</div>
+<div class="col-xs-12">
+<h1 class="home-content-choice">Êtes vous un commerçant ou un visiteur ?</h1>
+<div class="home-content-button">
+
+
+<button class="home-content-button-1" type="button" name="button">visiteur</button>
+<button class="home-content-button-1" type="button" name="button">commerçant</button>
+</div>
+</div>
+</div>
+  </div>
+</section>
+<?php include('footer.php'); ?>
---
+++ leisure.php
@@ -0,0 +1,57 @@
+<?php include('header.php'); ?>
+<?php include('footer.php'); ?>
+<section class="leisure">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 leisure-content">
+      <div class="leisure-content-img">
+        <img class="leisure-content-img-logo" src="../assets/img/zakera.jpg" alt="">
+      </div>
+      </div>
+      <div class="col-xs-12 leisure-content">
+      <div class="leisure-content-text">
+        <h1 class="leisure-content-text-title">La Place Zakera le coeur d'iscaryon </h1>
+        <p class="leisure-content-text-description">La place Zakera est l’endroit où des marchands issue des 4 coins de la galaxie viennent proposer leurs marchandises chaques jours de nouveaux marchands arrive et repartent alors profiter-en !</p>
+      </div>
+      </div>
+      <div class="col-xs-12  leisure-content">
+      <div class="leisure-content-img">
+        <img class="leisure-content-img-logo" src="../assets/img/otarion.jpg" alt="">
+      </div>
+      </div>
+      <div class="col-xs-12 leisure-content">
+      <div class="leisure-content-text">
+        <h1 class="leisure-content-text-title">La place otarion l’esprit d’iscaryon </h1>
+        <p class="leisure-content-text-description">Vous avez eu une journée difficile ? Vous êtes en vacances ou tout simplement en week-end ?
+Venez vous détendre et vous amusez à la place Otarion !</p>
+      </div>
+      </div>
+      <div class="leisure-content-choice">
+
+
+    <div class="leisure-content-choice-one">
+      <div class="leisure-content-choice-img">
+        <p class="leisure-content-choice-text">Restaurant</p>
+      <a class="leisure-content-choice-lien" href="restaurant.php">  <img  class="leisure-content-choice-img-logo"src="../assets/img/restaurant.jpg" alt="Restaurant"></a>
+      </div>
+ <div class="leisure-content-choice-img">
+   <p class="leisure-content-choice-text">Bar</p>
+  <a class="leisure-content-choice-lien" href="bar.php"><img  class="leisure-content-choice-img-logo"src="../assets/img/Bar.jpg" alt="Bar"></a>
+ </div>
+    </div>
+
+
+    <div class="leisure-content-choice-one">
+      <div class="leisure-content-choice-img">
+         <p class="leisure-content-choice-text">Bibiothèque</p>
+        <a class="leisure-content-choice-lien" href="library.php"><img  class="leisure-content-choice-img-logo"src="../assets/img/library.jpg" alt="library"></a>
+      </div>
+ <div class="leisure-content-choice-img">
+   <p class="leisure-content-choice-text">Terrain de foot</p>
+  <a class="leisure-content-choice-lien" href="ground.php"><img  class="leisure-content-choice-img-logo"src="../assets/img/stade.jpg" alt="ground"></a>
+ </div>
+    </div>
+      </div>
+    </div>
+  </div>
+</section>
