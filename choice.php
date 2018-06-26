ce8be8ea3dcd2b5f4973b9d03afacc078224ab94
joel2035 <joel.pokam@hetic.net>
2018-06-26 13:21:19 +0200

    choice & way

---
+++ choice.php
@@ -0,0 +1,20 @@
+<?php include('way.php'); ?>
+<section class="choice">
+  <div class="container">
+    <div class="row choice-content">
+      <form class="choice-content" action="" method="post">
+
+
+   <p class="choice-content-input"> <input class="choice-input" type="text" name="flood" placeholder = "Alimentaires" value=""> </p>
+   <p class="choice-content-input"> <input class="choice-input" type="text" name="technologie" placeholder = "Technologies" value=""> </p>
+   <p class="choice-content-input"> <input class="choice-input" type="text" name="precious stones" placeholder = "Pierres precieuses" value=""> </p>
+   <p class="choice-content-input"> <input class="choice-input" type="text" name="clothing" placeholder = "Vestimentaires" value=""> </p>
+</form>
+
+
+
+
+    </div>
+  </div>
+
+</section>
---
+++ way.php
@@ -0,0 +1,33 @@
+<?php include('header.php'); ?>
+
+<section class="way">
+  <div class="container">
+    <div class="row">
+      <div class="way-content">
+        <div class="way-contentOne one">
+          <p>1</p>
+        </div>
+        <div class="way-bar">
+
+        </div>
+        <div class="way-contentOne">
+          <p>2</p>
+        </div>
+        <div class="way-bar">
+
+        </div>
+        <div class="way-contentOne">
+          <p>3</p>
+        </div>
+        <div class="way-bar">
+
+        </div>
+        <div class="way-contentOne">
+          <p>4</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+ <?php include('footer.php'); ?>
